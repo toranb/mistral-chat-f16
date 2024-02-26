9ebf3ae77fddd19902bc08d774b3f1147247b261
toran billups <toranb@gmail.com>
2024-02-26 06:26:40 -0600

    added default backend config for Nx

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,11 @@
 import Config
 
+config :nx, :default_backend, EXLA.Backend
+
+config :exla, :clients,
+  cuda: [platform: :cuda, preallocate: false],
+  host: [platform: :host]
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
